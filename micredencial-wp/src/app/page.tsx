4cada44186b10d205427425f32a933d8455616df
CristianSoryu <cristianmanuelgonzalest@gmail.com>
2026-02-20 15:06:23 -0500

    changed to react native framework

--- micredencial-wp/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (<div>
-    <h1>Welcome to Micredencial WP</h1>
-    <p>This is the home page of your WordPress application built with Next.js.</p>
-  </div>
-  );
-}
--- micredencial-wp/src/pages/about.jsx
+++
@@ -1,8 +0,0 @@
-export default function about() {
-  return (
-    <div>
-      <h1>About MiCredencial WP</h1>
-      <p>MiCredencial WP is a WebPage application built with Next.js. It provides a seamless experience for managing your credentials and digital identity.</p>
-    </div>
-  );
-}
